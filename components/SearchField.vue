fff67bb0772166358d6b2671b7159e35d3d79306
Agustin Amato <agustin.amato96@gmail.com>
2021-06-17 01:15:59 -0300

    search-field: box-shadow style

--- components/SearchField.vue
+++ components/SearchField.vue
@@ -1,14 +1,15 @@
 <template>
   <v-text-field
+    class="custom-input"
     prepend-inner-icon="mdi-magnify"
     label="Search"
     solo
   ></v-text-field>
 </template>
 
-<style>
-.v-input__slot {
-  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.04) !important;
-  border-radius: 5px !important;
+<style lang="scss">
+#app .v-input.custom-input.v-text-field .v-input__slot {
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 5px;
 }
 </style>
