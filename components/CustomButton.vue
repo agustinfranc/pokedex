7f67bd6550d8fd1d31eff9bebf5c3504d7b5266b
Agustin Amato <agustin.amato96@gmail.com>
2021-06-16 23:24:20 -0300

    [component] custom-button

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-btn color="primary" rounded>
+  <v-btn :nuxt="to" :to="to" color="primary" rounded>
     <slot />
   </v-btn>
 </template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
