7addf865f9999803114d4b16eee5d0b171572203
Agustin Amato <agustin.amato96@gmail.com>
2021-06-17 01:02:11 -0300

    [component] search-field

---
+++ components/SearchField.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-text-field
+    prepend-inner-icon="mdi-magnify"
+    label="Search"
+    solo
+  ></v-text-field>
+</template>
+
+<style>
+.v-input__slot {
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.04) !important;
+  border-radius: 5px !important;
+}
+</style>
