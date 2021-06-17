464115ce327eed0517228a67e8fba854dde57a65
Agustin Amato <agustin.amato96@gmail.com>
2021-06-16 22:42:33 -0300

    set theme to light and primary color

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,18 +50,19 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
+    treeShake: true,
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+        light: {
+          primary: '#F22539',
+          // accent: colors.grey.darken3,
+          // secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+          // warning: colors.amber.base,
+          // error: colors.deepOrange.accent4,
+          // success: colors.green.accent3,
         },
       },
     },
