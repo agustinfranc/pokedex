381154c5121dc273a4a6a6d5eed4536875c63b4c
Agustin Amato <agustin.amato96@gmail.com>
2021-06-17 01:01:52 -0300

    [layout] blank

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,9 +1,17 @@
 <template>
   <v-app light>
     <v-main>
-      <v-container class="px-md-16">
+      <v-container class="pt-9 px-7 mw-md-main-content">
         <nuxt />
       </v-container>
     </v-main>
   </v-app>
 </template>
+
+<style>
+@media (min-width: 960px) {
+  .v-application .mw-md-main-content {
+    max-width: 55%;
+  }
+}
+</style>
