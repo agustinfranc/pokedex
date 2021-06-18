667a4c20688d4c301b663137678e9810170bff7e
Agustin Amato <agustin.amato96@gmail.com>
2021-06-17 21:53:25 -0300

    [store] initialize store and set favouritePokemons state

---
+++ store/index.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  favouritePokemons: [],
+})
+
+export const mutations = {
+  ADD_FAVOURITE_POKEMON(state, payload) {
+    state.favouritePokemons.push(payload)
+  },
+
+  REMOVE_FAVOURITE_POKEMON(state, payload) {
+    state.favouritePokemons.splice(payload)
+  },
+}
+
+export const actions = {
+  toggleFavouritePokemon({ commit, state }, payload) {
+    const index = state.favouritePokemons.findIndex(
+      (item) => item.name === payload.name
+    )
+
+    if (index === -1) {
+      commit('ADD_FAVOURITE_POKEMON', payload)
+
+      return
+    }
+
+    commit('REMOVE_FAVOURITE_POKEMON', index)
+  },
+}
