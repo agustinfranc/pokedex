0dbf15e8edbffaa7550cc3b22bc0e14e5a91f600
Agustin Amato <agustin.amato96@gmail.com>
2021-06-17 22:08:03 -0300

    [search-field] added prop and emitter

--- components/SearchField.vue
+++ components/SearchField.vue
@@ -4,8 +4,18 @@
     prepend-inner-icon="mdi-magnify"
     label="Search"
     solo
+    :value="value"
+    @input="$emit('input', $event)"
   ></v-text-field>
 </template>
+
+<script>
+export default {
+  props: {
+    value: String,
+  },
+}
+</script>
 
 <style lang="scss">
 #app .v-input.custom-input.v-text-field .v-input__slot {
